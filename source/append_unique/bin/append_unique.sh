e7d3568ae8e0a033d22e9b4b161f9e647fba6cc5
Morteza Jamali <mortezajamali4241@gmail.com>
2021-10-11 08:44:15 +0000

    fix(source): use absolute path in 'append_unique.sh' module

--- source/append_unique/bin/append_unique.sh
+++ source/append_unique/bin/append_unique.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+STR_EXIST_ROOT_PATH=$(realpath $(dirname $0)/../../str_exist)
+
+[ ! -z $STR_EXIST_ROOT ] && STR_EXIST_ROOT_PATH=$STR_EXIST_ROOT
+
 # $1 is target string
 # $2 is target file
-./str_exist/bin/str_exist.sh $1 $2 || echo "$1" >> "$2"
+[ ! -f "$2" ] && touch "$2"
+$STR_EXIST_ROOT_PATH/bin/str_exist.sh "$1" "$2" || echo "$1" >> "$2"
