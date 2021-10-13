66e2963c62c51d577dcdc5903a35d010a92e821d
Morteza Jamali <mortezajamali4241@gmail.com>
2021-10-13 06:06:10 +0000

    feat(script): create 'generate_docs()' function in 'document.sh' script

--- script/document.sh
+++ script/document.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-if ! command -v shdoc &> /dev/null; then
-  echo "ERROR:: 'shdoc' is not installed !" && exit 1
+ROOT_DIR=$(realpath $(dirname $0)/..)
+SOURCE_DIR=$ROOT_DIR/source
+DOCS_DIR=$ROOT_DIR/docs
+STR_EXIST_ROOT=$SOURCE_DIR/str_exist
+
+. $SOURCE_DIR/is_installed/bin/is_installed.sh
+. $SOURCE_DIR/append_unique/bin/append_unique.sh
+
+shdoc_status=0
+
+if [[ $(is_installed shdoc) != 0 ]]; then
+  shdoc_status=1
+  echo "ERROR:: 'shdoc' is not installed !"
 fi
 
+clean_docs() {
+  echo -n > $DOCS_DIR/*.md
+}
+
+list_scripts() {
+  append_unique "# List of scripts" $DOCS_DIR/scripts.md
+  append_unique "1. [$(basename $1)](../$1/README.md)" $DOCS_DIR/scripts.md
+}
+
+generate_docs() {
+  [ $shdoc_status -eq 0 ] && \
+  [ -d $1/bin ] && shdoc < $1/bin/* > $1/README.md
+}
+
+clean_docs
+
 for s in ./source/*; do
-  [ -d $s/bin ] && shdoc < $s/bin/* > $s/README.md
+  list_scripts $s
+  generate_docs $s
 done
