62544e76f0ebf015f3a11ab239ed3795e2777f2b
Morteza Jamali <mortezajamali4241@gmail.com>
2022-04-19 18:37:50 +0000

    fix(packages): fix dependency loading for `color` and `log`

--- packages/color/test/test_color.sh
+++ packages/color/test/test_color.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
 
+# Enable/disable shell options
+shopt -s expand_aliases
+
+# Constants
 ROOT_PATH=$(dirname $(realpath $(dirname $0)))
 
-source $ROOT_PATH/color.sh
+# Aliases
+alias color=$ROOT_PATH/color
 
 # Background colors test
-echo -e $(color_bg red "Red background")
-color_bg yellow "Yellow background" -e
+echo -e $(color bg red "Red background")
+color bg yellow "Yellow background" -e
 
-echo -n "Lorem ipsum $(color_bg yellow 'dolor sit amet' -e), consectetur adipiscing elit."
-echo "Nullam non felis metus. $(color_bg green 'Sed eleifend elit eu' -e) turpis faucibus finibus. Cras et venenatis arcu."
+echo -n "Lorem ipsum $(color bg yellow 'dolor sit amet' -e), consectetur adipiscing elit."
+echo "Nullam non felis metus. $(color bg green 'Sed eleifend elit eu' -e) turpis faucibus finibus. Cras et venenatis arcu."
 
 echo
 
 # Forground colors test
-echo -e $(color_fg red "Red text")
-color_fg yellow "Yellow text" -e
+echo -e $(color fg red "Red text")
+color fg yellow "Yellow text" -e
 
-echo -n "Lorem ipsum $(color_fg yellow 'dolor sit amet' -e), consectetur adipiscing elit."
-echo "Nullam non felis metus. $(color_fg green 'Sed eleifend elit eu' -e) turpis faucibus finibus. Cras et venenatis arcu."
+echo -n "Lorem ipsum $(color fg yellow 'dolor sit amet' -e), consectetur adipiscing elit."
+echo "Nullam non felis metus. $(color fg green 'Sed eleifend elit eu' -e) turpis faucibus finibus. Cras et venenatis arcu."
 
 echo
 
 # Forground and background test
-color yellow blue "Yellow background and blue forground" -e
+color fb yellow blue "Yellow background and blue forground" -e
--- packages/log/test/test_log.sh
+++ packages/log/test/test_log.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+shopt -s expand_aliases
+
 ROOT_PATH=$(dirname $(realpath $(dirname $0)))
 
-source $ROOT_PATH/log.sh
+alias log=$ROOT_PATH/log
 
-log_error "This is a simple error message."
-log_warn "This is a simple warn message."
-log_success "This is a simple success message."
-log_info "This is a simple info message."
+log error "This is a simple error message."
+log warn "This is a simple warn message."
+log success "This is a simple success message."
+log info "This is a simple info message."
