dd31c4666ffcad78a4893a045e84941af12de10e
Morteza Jamali <mortezajamali4241@gmail.com>
2022-04-25 21:09:57 +0000

    chore(packages): move scripts from `source` to `packages` directory

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
-if ! command -v gawk &> /dev/null; then
-  sudo apt install gawk
+ROOT_PATH=$(dirname $(realpath $(dirname $0)))
+
+alias log=$ROOT_PATH/packages/log/log
+
+# Install Shellspec
+if command -v shellspec >/dev/null 2>&1; then
+  log info "Shellspec is already installed"
+else
+  ( curl -fsSL https://git.io/shellspec | sh -s -- --yes ) && \
+  echo "export PATH=$HOME/.local/bin:\$PATH" >> ~/.bashrc && \
+  log success "Shellspec installed"
 fi
 
-# Install shdoc
-if ! command -v shdoc &> /dev/null; then
-  mkdir tmp && cd tmp && \
-  git clone --recursive https://github.com/reconquest/shdoc && \
-  cd shdoc && sudo make install
-fi
+# if ! command -v gawk &> /dev/null; then
+#   sudo apt install gawk
+# fi
 
-echo "Development environment is ready !"
+# # Install shdoc
+# if ! command -v shdoc &> /dev/null; then
+#   mkdir tmp && cd tmp && \
+#   git clone --recursive https://github.com/reconquest/shdoc && \
+#   cd shdoc && sudo make install
+# fi
+
+# echo "Development environment is ready !"
