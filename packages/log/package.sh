4211f4b7f9aa92cbf4193e0aedfaf55d27a16ee3
Morteza Jamali <mortezajamali4241@gmail.com>
2022-08-11 04:55:00 +0000

    chore(packages): create `package.sh` in `log` package

---
+++ packages/log/package.sh
@@ -0,0 +1,21 @@
+NAME="morajlab/color"
+DESCRIPTION="A mini logger script"
+VERSION="0.0.1-alpha"
+DEPENDENCIES=(
+  ["morajlab/color"]="*"
+)
+LICENSE="MIT"
+KEYWORDS=(
+  "bash"
+  "script"
+  "color"
+  "log"
+  "unix"
+  "linux"
+)
+AUTHOR=(
+  ["name"]="Morteza Jamali"
+  ["email"]="mortezajamali4241@gmail.com"
+  ["website"]="https://moraj.ir"
+)
+REPOSITORY="https://github.com/morajlab/bash-scripts"
